480d2f097144990d51650f55ac860dfc32c6fdcd
Taylor Masiello <masiellotay@gmail.com>
2026-03-21 10:52:55 -0400

    updated to a randomly generated heart beat btween 55 and 115 inclusive

--- src/hooks/heartBeat.tsx
+++ src/hooks/heartBeat.tsx
@@ -1,15 +1,26 @@
 import React, {useState, useEffect } from 'react';
 
+function getRandomBMP(min: number, max: number){ //cannot declare as int, all integers are "numbers"
+  var bpm = Math.random();
+
+  return Math.floor(bpm * ((max - min + 1) + min)); // +1 makes it inclusive ; + min bumps math.random from 1 to the min value
+
+}
+
 export function useHeartBeat() {
-  const [beat, setBeat] = useState(0);
+  const [beat, setBeat] = useState(getRandomBMP(55, 115));
+  //const timer = setTimeout(() => )
+
+  //changed beat + 1 to a random number between 60 and 100
+
 
   useEffect (() => {
-    setTimeout(() => {
-      setBeat((beat) => beat + 1);
+    const beatTimer = setTimeout(() => {
+      setBeat(( getRandomBMP(55, 115))); //sets heartbeat to a random number between 60-125 inclusive
     }, 1000);
 
-    return () => clearTimeout(beat);
-  });
+    return () => clearTimeout(beatTimer);
+  }, [beat]);
 
   return {beat}
 }
