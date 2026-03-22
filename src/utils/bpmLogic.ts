21b6ae9a643cb84c4b598784a3af3acef2a65096
Taylor Masiello <masiellotay@gmail.com>
2026-03-22 04:52:32 -0400

    isBPMDanger refactor complete minus comments clean up

--- src/utils/bpmLogic.ts
+++ src/utils/bpmLogic.ts
@@ -19,53 +19,51 @@ export function getRandomBMP(min: number, max: number){
 
 //refactor to accept obj like {isAthlete: boolean}
 export function isBPMDanger (currentBpm: number, inputs: UserInputs) { //isAthlete is in UserInputs ; declaration
+  
+  let dangerLevel: DangerLevel = 'NONE';
+
   let isAthlete = inputs.isAthlete;
   let isExercising = inputs.isExercising;
 
-  let dangerLevel: DangerLevel = 'NONE';
+  let udrAthFl = (currentBpm < bpmAthleteFloor );
+  let udrExCeil = (currentBpm < bmpExerciseCeil);
+  let udrNomFl = (currentBpm < bpmNominalFloor);
+  let udrNomCeil = (currentBpm < bpmNominalCeil);
 
-  //isAth_orEx_and_OvrExCl_orUdrAthFl
-  let athelteExercisingEMG = ((isAthlete || isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmAthleteFloor));
-  //isAth_andNotEx_and_UdrNormCl_orUdrAthFl
-  let athelteRestEMG = ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteFloor));
-  //notAth_andisEx_and_UdrExCl_orUdrNormFl
-  let nonAthelteExercisingEMG = ((!isAthlete && isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmNominalFloor));
-  //notAth_andNotEx_and_Udr_NormCl_orUdrNormFl
-  let nonAthleteRestEMG = ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmNominalFloor));
+  let ovrNomCeil = (currentBpm > bpmNominalCeil);
+  let ovrAthFl = (currentBpm > bpmAthleteFloor);
+
+  //let athelteExercisingEMG = ((isAthlete || isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmAthleteFloor));
+  //let athelteRestEMG = ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteFloor  /*overAthFl*/));
+  //let nonAthelteExercisingEMG = ((!isAthlete && isExercising) && (currentBpm < bmpExerciseCeil /*overExCeil*/ || currentBpm < bpmNominalFloor /*overNomFl*/));
+  //let nonAthleteRestEMG = ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil /*overNomCeil*/ || currentBpm < bpmNominalFloor));
+ 
+  let athelteExercisingEMG = ((isAthlete || isExercising) && (udrExCeil || udrAthFl));
+  let athelteRestEMG = ((isAthlete && !isExercising) && (udrNomCeil || udrAthFl));
+  let nonAthelteExercisingEMG = ((!isAthlete && isExercising) && (udrExCeil || udrNomFl));
+  let nonAthleteRestEMG = ((!isAthlete && !isExercising) && (udrNomCeil || udrNomFl));
 
   if (athelteExercisingEMG || athelteRestEMG || nonAthelteExercisingEMG || nonAthleteRestEMG) {
     dangerLevel = 'EMG';
-  }    
+  }
+  
+  //let ovrNomCeil = (currentBpm > bpmNominalCeil);
 
-//   if (((isAthlete || isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmAthleteFloor)) || 
-//         ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteFloor)) || 
-//         ((!isAthlete && isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmNominalFloor)) || 
-//         ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmNominalFloor)) ||) {
-//       dangerLevel = 'EMG';
+//   if ((currentBpm > bpmNominalCeil && currentBpm < bmpExerciseCeil && isExercising) || 
+//         (currentBpm < bpmNominalFloor && currentBpm > bpmAthleteFloor && isAthlete)) {
+//      dangerLevel = 'HIGH'; //med priority
+//      return dangerLevel;
 //   }
 
-  if ((currentBpm > bpmNominalCeil && currentBpm < bmpExerciseCeil && isExercising) || 
-        (currentBpm < bpmNominalFloor && currentBpm > bpmAthleteFloor && isAthlete)) {
+  if ((ovrNomCeil && udrExCeil && isExercising) || (udrNomFl && ovrAthFl && isAthlete)) {
      dangerLevel = 'HIGH'; //med priority
      return dangerLevel;
   }
 
-  if (currentBpm < (bpmNominalCeil - 1) || currentBpm < (bpmNominalFloor + 1)) {
+  if ((currentBpm <= bpmNominalCeil) && (currentBpm >= bpmNominalFloor)) {
       return dangerLevel = 'NONE'; //lowest priority
   }
 
-  
+  return 'NONE'; //default return if there is some error; catch block
 
-  //will return more than just bool ; 3 levels of "danger"
-  //return inDangerZone
-  //return dangerLevel;
 }
-
-//furture buzzBuzz() logic
-  //if (bpm > 100 && bpm < 135 || bpm < 60 && bpm > 40) {
-  //} else if (bpm > 135 || bpm < 40) {
-    //await triggerDangerBuzz(5);
-   // super red styling
-  //if (bpm < 60 && isAthlete || bpm > 100 && isExercising) {
-    //no trigger DangerBuzz
-  //}
