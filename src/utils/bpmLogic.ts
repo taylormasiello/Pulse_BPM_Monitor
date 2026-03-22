fa89f5381d14d3f66e46c03442e1ab0a3e10525c
Taylor Masiello <masiellotay@gmail.com>
2026-03-22 04:26:16 -0400

    in progress danger level conditional logic refactor (readability and better coding practices and scalability)

--- src/utils/bpmLogic.ts
+++ src/utils/bpmLogic.ts
@@ -7,8 +7,8 @@ export const bmpDangerFloor = 195;
 const bpmNominalFloor = 60;
 const bpmNominalCeil = 100;
 
-const bpmAthleteMin = 40;
-const bmpExerciseMax = 150; 
+const bpmAthleteFloor = 40;
+const bmpExerciseCeil = 150; 
 //^more or less an "avg" for "highest safe bpm during exercise" for folks around 40-45 years old
 //^^an estimaged average point of potential users, given app is a "health monitor"; less likely to be used by younger users
 
@@ -19,32 +19,46 @@ export function getRandomBMP(min: number, max: number){
 
 //refactor to accept obj like {isAthlete: boolean}
 export function isBPMDanger (currentBpm: number, inputs: UserInputs) { //isAthlete is in UserInputs ; declaration
-
-
-  
-  let dangerLevel: DangerLevel = 'NONE';
   let isAthlete = inputs.isAthlete;
   let isExercising = inputs.isExercising;
 
+  let dangerLevel: DangerLevel = 'NONE';
+
+  //isAth_orEx_and_OvrExCl_orUdrAthFl
+  let athelteExercisingEMG = ((isAthlete || isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmAthleteFloor));
+  //isAth_andNotEx_and_UdrNormCl_orUdrAthFl
+  let athelteRestEMG = ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteFloor));
+  //notAth_andisEx_and_UdrExCl_orUdrNormFl
+  let nonAthelteExercisingEMG = ((!isAthlete && isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmNominalFloor));
+  //notAth_andNotEx_and_Udr_NormCl_orUdrNormFl
+  let nonAthleteRestEMG = ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmNominalFloor));
+
+  if (athelteExercisingEMG || athelteRestEMG || nonAthelteExercisingEMG || nonAthleteRestEMG) {
+    dangerLevel = 'EMG';
+  }    
+
+//   if (((isAthlete || isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmAthleteFloor)) || 
+//         ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteFloor)) || 
+//         ((!isAthlete && isExercising) && (currentBpm < bmpExerciseCeil || currentBpm < bpmNominalFloor)) || 
+//         ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmNominalFloor)) ||) {
+//       dangerLevel = 'EMG';
+//   }
+
+  if ((currentBpm > bpmNominalCeil && currentBpm < bmpExerciseCeil && isExercising) || 
+        (currentBpm < bpmNominalFloor && currentBpm > bpmAthleteFloor && isAthlete)) {
+     dangerLevel = 'HIGH'; //med priority
+     return dangerLevel;
+  }
+
   if (currentBpm < (bpmNominalCeil - 1) || currentBpm < (bpmNominalFloor + 1)) {
-      dangerLevel = 'NONE';
+      return dangerLevel = 'NONE'; //lowest priority
   }
 
-  if ((currentBpm > bpmNominalCeil && currentBpm < bmpExerciseMax && isExercising) || 
-        (currentBpm < bpmNominalFloor && currentBpm > bpmAthleteMin && isAthlete)) {
-     dangerLevel = 'HIGH';
-  }
-
-  if (((isAthlete || isExercising) && (currentBpm < bmpExerciseMax || currentBpm < bpmAthleteMin)) || 
-        ((isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmAthleteMin)) || 
-        ((!isAthlete && isExercising) && (currentBpm < bmpExerciseMax || currentBpm < bpmNominalFloor)) || 
-        ((!isAthlete && !isExercising) && (currentBpm < bpmNominalCeil || currentBpm < bpmNominalFloor)) ||) {
-      dangerLevel = 'EMG';
-  }
+  
 
   //will return more than just bool ; 3 levels of "danger"
   //return inDangerZone
-  return dangerLevel;
+  //return dangerLevel;
 }
 
 //furture buzzBuzz() logic
